ce6fddf051d6114ac68f0203b1070ba3d537545e
J-DiegoSr <jdiego.sr96@gmail.com>
2025-01-13 17:35:17 -0500

    Probando Commit ota vez

--- src/Principal.java
+++ src/Principal.java
@@ -1,6 +1,6 @@
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo todo excelente?");
-        System.out.println("Confirmen?");
+        System.out.println("Confirmen? ok");
     }
 }
